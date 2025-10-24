1b346d535e7804acafeae912142de8dfb98e8d24
Stephen Tetley <stephen.tetley@gmail.com>
2025-10-24 22:03:34 +0100

    equi_compare - work towards final report

--- Scripts/equi_compare/07_create_report_output_table.sql
+++ Scripts/equi_compare/07_create_report_output_table.sql
@@ -1,4 +1,4 @@
-CREATE OR REPLACE MACRO pp_compare_status(cstatus) AS 
+CREATE OR REPLACE TEMPORARY MACRO pp_compare_status(cstatus) AS 
 CASE 
     WHEN cstatus = 'in_both'::compare_status THEN 'In both'
     WHEN cstatus = 'no_pli_aib_ref'::compare_status THEN 'Missing AIB pli reference'
@@ -7,7 +7,7 @@ CASE
     ELSE 'Unknown'
 END;
 
-CREATE OR REPLACE MACRO compare_dates(s4_date, ai2_date) AS 
+CREATE OR REPLACE TEMPORARY MACRO compare_dates(s4_date, ai2_date) AS 
 CASE 
     WHEN s4_date IS NULL THEN {'s4': null, 'ai2': strftime(ai2_date, '%d.%m.%Y')}
     WHEN ai2_date IS NULL THEN {'s4': strftime(s4_date, '%d.%m.%Y'), 'ai2': null}
@@ -15,28 +15,112 @@ CASE
     ELSE {'s4': strftime(s4_date, '%d.%m.%Y'), 'ai2': strftime(ai2_date, '%d.%m.%Y')}
 END;
 
+CREATE OR REPLACE TEMPORARY MACRO compare_status(s4_status, ai2_status) AS 
+CASE 
+    WHEN s4_status IS NULL THEN {'s4': null, 'ai2': ai2_status}
+    WHEN ai2_status IS NULL THEN {'s4': s4_status, 'ai2': null}
+    WHEN s4_status LIKE 'OPER%' AND ai2_status = 'OPERATIONAL' THEN {'s4': s4_status, 'ai2': '**'}
+    WHEN s4_status LIKE 'NOP%' AND ai2_status = 'NON OPERATIONAL' THEN {'s4': s4_status, 'ai2': '**'}
+    WHEN s4_status LIKE 'DCOM%' AND ai2_status = 'DECOMMISSIONED' THEN {'s4': s4_status, 'ai2': '**'}
+    WHEN s4_status LIKE 'OOS%' AND ai2_status = 'OUT OF SERVICE' THEN {'s4': s4_status, 'ai2': '**'}
+    WHEN s4_status LIKE 'DISP%' AND ai2_status = 'DISPOSED OF' THEN {'s4': s4_status, 'ai2': '**'}
+    WHEN s4_status LIKE 'TBCM%' AND ai2_status = 'TO BE COMMISSIONED' THEN {'s4': s4_status, 'ai2': '**'}
+    ELSE {'s4': s4_status, 'ai2': ai2_status}
+END;
+
+
+CREATE OR REPLACE TEMPORARY MACRO compare_manufacturer(s4_manuf, ai2_manuf) AS 
+CASE 
+    WHEN s4_manuf IS NULL THEN {'s4': null, 'ai2': ai2_manuf}
+    WHEN ai2_manuf IS NULL THEN {'s4': s4_manuf, 'ai2': null}
+    WHEN s4_manuf = ai2_manuf THEN {'s4': s4_manuf, 'ai2': '**'}
+    ELSE {'s4': s4_manuf, 'ai2': ai2_manuf}
+END;
+
+CREATE OR REPLACE TEMPORARY MACRO compare_model(s4_model, ai2_model) AS 
+CASE 
+    WHEN s4_model IS NULL THEN {'s4': null, 'ai2': ai2_model}
+    WHEN ai2_model IS NULL THEN {'s4': s4_model, 'ai2': null}
+    WHEN s4_model = ai2_model THEN {'s4': s4_model, 'ai2': '**'}
+    ELSE {'s4': s4_model, 'ai2': ai2_model}
+END;
+
+CREATE OR REPLACE TEMPORARY MACRO compare_specific_model(s4_specific_model, ai2_specific_model) AS 
+CASE 
+    WHEN s4_specific_model IS NULL THEN {'s4': null, 'ai2': ai2_specific_model}
+    WHEN ai2_specific_model IS NULL THEN {'s4': s4_specific_model, 'ai2': null}
+    WHEN s4_specific_model = ai2_specific_model THEN {'s4': s4_specific_model, 'ai2': '**'}
+    ELSE {'s4': s4_specific_model, 'ai2': ai2_specific_model}
+END;
+
+CREATE OR REPLACE TEMPORARY MACRO compare_serial_number(s4_serial_number, ai2_serial_number) AS 
+CASE 
+    WHEN s4_serial_number IS NULL THEN {'s4': null, 'ai2': ai2_serial_number}
+    WHEN ai2_serial_number IS NULL THEN {'s4': s4_serial_number, 'ai2': null}
+    WHEN s4_serial_number = ai2_serial_number THEN {'s4': s4_serial_number, 'ai2': '**'}
+    ELSE {'s4': s4_serial_number, 'ai2': ai2_serial_number}
+END;
+
+CREATE OR REPLACE TEMPORARY MACRO compare_pandi_tag(s4_pandi_tag, ai2_pandi_tag) AS 
+CASE 
+    WHEN s4_pandi_tag IS NULL THEN {'s4': null, 'ai2': ai2_pandi_tag}
+    WHEN ai2_pandi_tag IS NULL THEN {'s4': s4_pandi_tag, 'ai2': null}
+    WHEN s4_pandi_tag = ai2_pandi_tag THEN {'s4': s4_pandi_tag, 'ai2': '**'}
+    ELSE {'s4': s4_pandi_tag, 'ai2': ai2_pandi_tag}
+END;
+
+CREATE OR REPLACE TEMPORARY MACRO compare_parent_ref(s4_parent_ref, ai2_parent_ref) AS 
+CASE 
+    WHEN s4_parent_ref IS NULL THEN {'s4': null, 'ai2': ai2_parent_ref}
+    WHEN ai2_parent_ref IS NULL THEN {'s4': s4_parent_ref, 'ai2': null}
+    WHEN s4_parent_ref = ai2_parent_ref THEN {'s4': s4_parent_ref, 'ai2': '**'}
+    ELSE {'s4': s4_parent_ref, 'ai2': ai2_parent_ref}
+END;
+
+
 
 CREATE OR REPLACE TABLE equi_compare.output_report AS
-SELECT 
-    t.site AS site_code,
-    coalesce(t.ai2_inst_name, t.s4_site_name) AS site_name,
-    coalesce(t.s4_floc, t.ai2_floc) AS floc,
-    t.s4_equi_id AS s4_equi_id,
-    t.pli_num AS pli_num, 
-    t.name_s4 AS name_s4,
-    t.name_ai2 AS name_ai2, 
-    pp_compare_status(t.compare_status) AS compare_status,
-    null AS diff_summary,
-    null AS diff_count,
-    t.s4_standard_class AS s4_standard_class,
-    t.ai2_equipment_type AS ai2_equipment_type,
-    compare_dates(t.install_date_s4, t.install_date_ai2) AS __date_ans,
-    struct_extract(__date_ans, 's4') AS install_date_s4,
-    struct_extract(__date_ans, 'ai2') AS install_date_ai2,
-    t.status_s4 AS status_s4,
-    t.status_ai2 AS status_ai2,    
-FROM equi_compare.output_report_source_all t;
+WITH cte AS (
+    SELECT 
+        t.site AS site_code,
+        coalesce(t.ai2_inst_name, t.s4_site_name) AS site_name,
+        coalesce(t.s4_floc, t.ai2_floc) AS floc,
+        t.s4_equi_id AS s4_equi_id,
+        t.pli_num AS pli_num, 
+        t.name_s4 AS name_s4,
+        t.name_ai2 AS name_ai2, 
+        pp_compare_status(t.compare_status) AS compare_status,
+        null AS diff_summary,
+        null AS diff_count,
+        t.s4_standard_class AS s4_standard_class,
+        t.ai2_equipment_type AS ai2_equipment_type,
+        compare_dates(t.install_date_s4, t.install_date_ai2) AS __ans_date,
+        struct_extract(__ans_date, 's4') AS install_date_s4,
+        struct_extract(__ans_date, 'ai2') AS install_date_ai2,
+        compare_status(t.status_s4, t.status_ai2) AS __ans_status,
+        struct_extract(__ans_status, 's4') AS status_s4,
+        struct_extract(__ans_status, 'ai2') AS status_ai2, 
+        compare_manufacturer(t.manufacturer_s4, t.manufacturer_ai2) AS __ans_manufacturer,
+        struct_extract(__ans_manufacturer, 's4') AS manufacturer_s4,
+        struct_extract(__ans_manufacturer, 'ai2') AS manufacturer_ai2,   
+        compare_model(t.model_s4, t.model_ai2) AS __ans_model,
+        struct_extract(__ans_model, 's4') AS model_s4,
+        struct_extract(__ans_model, 'ai2') AS model_ai2,
+        compare_specific_model(t.specific_model_s4, t.specific_model_ai2) AS __ans_specific_model,
+        struct_extract(__ans_specific_model, 's4') AS specific_model_s4,
+        struct_extract(__ans_specific_model, 'ai2') AS specific_model_ai2,
+        compare_serial_number(t.serial_number_s4, t.serial_number_ai2) AS __ans_serial_number,
+        struct_extract(__ans_serial_number, 's4') AS serial_number_s4,
+        struct_extract(__ans_serial_number, 'ai2') AS serial_number_ai2,
+        compare_pandi_tag(t.pandi_tag_s4, t.pandi_tag_ai2) AS __ans_pandi_tag,
+        struct_extract(__ans_pandi_tag, 's4') AS pandi_tag_s4,
+        struct_extract(__ans_pandi_tag, 'ai2') AS pandi_tag_ai2,
+        compare_parent_ref(t.parent_ref_s4, t.parent_ref_ai2) AS __ans_parent_ref,
+        struct_extract(__ans_parent_ref, 's4') AS parent_ref_s4,
+        struct_extract(__ans_parent_ref, 'ai2') AS parent_ref_ai2,
+    FROM equi_compare.output_report_source_all t
+) 
+SELECT COLUMNS(c -> c NOT LIKE '__ans_%') FROM cte;
 
-SELECT * FROM equi_compare.output_report 
-ORDER BY site_code;
+
     
