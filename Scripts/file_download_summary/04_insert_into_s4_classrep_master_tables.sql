9f68cf339efb20cc701f7ccc481dbb63cc22e282
Stephen Tetley <stephen.tetley@gmail.com>
2025-10-27 18:02:49 +0000

    file_download_summary using PIVOT for asset condition

--- Scripts/file_download_summary/04_insert_into_s4_classrep_master_tables.sql
+++ Scripts/file_download_summary/04_insert_into_s4_classrep_master_tables.sql
@@ -1,4 +1,13 @@
 DELETE FROM s4_classrep.floc_masterdata;
+DELETE FROM s4_classrep.equi_masterdata;
+DELETE FROM s4_classrep.floc_aib_reference;
+DELETE FROM s4_classrep.equi_aib_reference;
+DELETE FROM s4_classrep.equi_asset_condition;
+DELETE FROM s4_classrep.floc_solution_id;
+DELETE FROM s4_classrep.equi_solution_id;
+DELETE FROM s4_classrep.floc_east_north;
+DELETE FROM s4_classrep.equi_east_north;
+
 
 INSERT OR REPLACE INTO s4_classrep.floc_masterdata BY NAME
 SELECT 
@@ -68,96 +77,122 @@ SELECT
 FROM file_download_landing.equi t;
 
 
---
+
 ---- ## AIB_REFERENCE (floc)
---
---INSERT OR REPLACE INTO s4_classrep.floc_aib_reference BY NAME
---SELECT DISTINCT ON (funcloc_id, value_index)
---    t.funcloc AS funcloc_id,
---    t.valcnt AS value_index,
---    t.atwrt AS ai2_aib_reference
---FROM file_download.valuafloc t
---WHERE t.charid = 'AI2_AIB_REFERENCE'
---AND ai2_aib_reference IS NOT NULL;
---
+
+INSERT OR REPLACE INTO s4_classrep.floc_aib_reference BY NAME
+SELECT DISTINCT ON (funcloc_id, value_index)
+    t.funcloc AS funcloc_id,
+    t.valcnt AS value_index,
+    t.atwrt AS ai2_aib_reference
+FROM file_download_landing.valuafloc t
+WHERE t.charid = 'AI2_AIB_REFERENCE'
+AND ai2_aib_reference IS NOT NULL;
+
 ---- ## AIB_REFERENCE (equi)
---
---INSERT OR REPLACE INTO s4_classrep.equi_aib_reference BY NAME
---SELECT DISTINCT ON (equipment_id, value_index)
---    t.equi AS equipment_id,
---    t.valcnt AS value_index,
---    t.atwrt AS ai2_aib_reference
---FROM file_download.valuaequi t
---WHERE t.charid = 'AI2_AIB_REFERENCE'
---AND ai2_aib_reference IS NOT NULL;
---
+
+INSERT OR REPLACE INTO s4_classrep.equi_aib_reference BY NAME
+SELECT DISTINCT ON (equipment_id, value_index)
+    t.equi AS equipment_id,
+    t.valcnt AS value_index,
+    t.atwrt AS ai2_aib_reference
+FROM file_download_landing.valuaequi t
+WHERE t.charid = 'AI2_AIB_REFERENCE'
+AND ai2_aib_reference IS NOT NULL;
+
 ---- ## ASSET_CONDITION
---
+
+
 ---- Don't add empty records so use a cte for filtering
---INSERT OR REPLACE INTO s4_classrep.equi_asset_condition BY NAME
---WITH cte AS (
---    SELECT DISTINCT ON(e.equipment_id)   
---        e.equipment_id AS equipment_id,
---        any_value(CASE WHEN eav.charid = 'CONDITION_GRADE' THEN eav.atwrt ELSE NULL END) AS condition_grade,
---        any_value(CASE WHEN eav.charid = 'CONDITION_GRADE_REASON' THEN eav.atwrt ELSE NULL END) AS condition_grade_reason,
---        any_value(CASE WHEN eav.charid = 'SURVEY_COMMENTS' THEN eav.atwrt ELSE NULL END) AS survey_comments,
---        any_value(CASE WHEN eav.charid = 'SURVEY_DATE' THEN TRY_CAST(eav.atflv AS INTEGER) ELSE NULL END) AS survey_date,
---    FROM s4_classrep.equi_masterdata e
---    JOIN file_download.valuaequi eav ON eav.equi = e.equipment_id
---    GROUP BY equipment_id
---)
---SELECT 
---    equipment_id,
---    condition_grade,
---    condition_grade_reason,
---    survey_comments,
---    survey_date,
---FROM cte
---WHERE condition_grade IS NOT NULL OR condition_grade_reason IS NOT NULL OR survey_comments IS NOT NULL OR survey_date IS NOT NULL; 
---
+INSERT OR REPLACE INTO s4_classrep.equi_asset_condition BY NAME
+WITH cte1 AS (
+    PIVOT file_download_landing.valuaequi
+    ON "CHARID" IN (
+        'CONDITION_GRADE',
+        'CONDITION_GRADE_REASON',
+    )
+    USING any_value("ATWRT")
+    GROUP BY "EQUI"
+), cte2 AS (
+    PIVOT file_download_landing.valuaequi
+    ON "CHARID" IN (
+        'SURVEY_DATE'
+    )
+    USING any_value("ATFLV")
+    GROUP BY "EQUI"
+), cte3 AS (
+SELECT 
+    t."EQUI" AS equipment_id,
+    t."CONDITION_GRADE" AS condition_grade,
+    t."CONDITION_GRADE_REASON" AS condition_grade_reason,
+    t1."SURVEY_DATE" AS survey_date,
+FROM cte1 t
+JOIN cte2 t1 ON t1."EQUI" = t."EQUI"
+)
+SELECT 
+    * 
+FROM cte3
+WHERE coalesce(condition_grade, condition_grade_reason, survey_date::VARCHAR) IS NOT NULL;
+
+
 ---- ## EAST_NORTH 
---
---INSERT OR REPLACE INTO s4_classrep.floc_east_north BY NAME
---SELECT DISTINCT ON(f.funcloc_id)   
---    f.funcloc_id AS funcloc_id,
---    any_value(CASE WHEN eav.charid = 'EASTING' THEN TRY_CAST(eav.atflv AS INTEGER) ELSE NULL END) AS easting,
---    any_value(CASE WHEN eav.charid = 'NORTHING' THEN TRY_CAST(eav.atflv AS INTEGER) ELSE NULL END) AS northing,
---FROM s4_classrep.floc_masterdata f
---JOIN file_download.valuafloc eav ON eav.funcloc = f.funcloc_id
---GROUP BY funcloc_id;
---
---INSERT OR REPLACE INTO s4_classrep.equi_east_north BY NAME
---SELECT DISTINCT ON(e.equipment_id)
---    e.equipment_id AS equipment_id,
---    any_value(CASE WHEN eav.charid = 'EASTING' THEN TRY_CAST(eav.atflv AS INTEGER) ELSE NULL END) AS easting,
---    any_value(CASE WHEN eav.charid = 'NORTHING' THEN TRY_CAST(eav.atflv AS INTEGER) ELSE NULL END) AS northing,
---FROM s4_classrep.equi_masterdata e
---JOIN file_download.valuaequi eav ON eav.equi = e.equipment_id
---GROUP BY equipment_id;
---
---
+
+INSERT OR REPLACE INTO s4_classrep.floc_east_north BY NAME
+WITH cte AS (
+    PIVOT file_download_landing.valuafloc
+    ON "CHARID" IN (
+        'EASTING',
+        'NORTHING',
+    )
+    USING any_value("ATFLV")
+    GROUP BY "FUNCLOC"
+)
+SELECT 
+    "FUNCLOC" AS funcloc_id,
+    "EASTING" AS easting,
+    "NORTHING" AS northing,
+FROM cte;
+
+
+INSERT OR REPLACE INTO s4_classrep.equi_east_north BY NAME
+WITH cte AS (
+    PIVOT file_download_landing.valuaequi
+    ON "CHARID" IN (
+        'EASTING',
+        'NORTHING',
+    )
+    USING any_value("ATFLV")
+    GROUP BY "EQUI"
+)
+SELECT 
+    "EQUI" AS equipment_id,
+    "EASTING" AS easting,
+    "NORTHING" AS northing,
+FROM cte;
+
+
 ---- ## SOLUTION_ID
---
---INSERT INTO s4_classrep.floc_solution_id BY NAME
---SELECT DISTINCT ON (funcloc_id, value_index)
---    t.funcloc AS funcloc_id,
---    t.valcnt AS value_index,
---    t.atwrt AS solution_id
---FROM file_download.valuafloc t
---WHERE t.charid = 'SOLUTION_ID'
---AND solution_id IS NOT NULL;
---
---
---INSERT INTO s4_classrep.equi_solution_id BY NAME
---SELECT DISTINCT ON (equipment_id, value_index)
---    t.equi AS equipment_id,
---    t.valcnt AS value_index,
---    t.atwrt AS solution_id
---FROM file_download.valuaequi t
---WHERE t.charid = 'SOLUTION_ID'
---AND solution_id IS NOT NULL;
---
---
+
+INSERT INTO s4_classrep.floc_solution_id BY NAME
+SELECT DISTINCT ON (funcloc_id, value_index)
+    t.funcloc AS funcloc_id,
+    t.valcnt AS value_index,
+    t.atwrt AS solution_id
+FROM file_download_landing.valuafloc t
+WHERE t.charid = 'SOLUTION_ID'
+AND solution_id IS NOT NULL;
+
+
+INSERT INTO s4_classrep.equi_solution_id BY NAME
+SELECT DISTINCT ON (equipment_id, value_index)
+    t.equi AS equipment_id,
+    t.valcnt AS value_index,
+    t.atwrt AS solution_id
+FROM file_download_landing.valuaequi t
+WHERE t.charid = 'SOLUTION_ID'
+AND solution_id IS NOT NULL;
+
+
 ---- ## Equi shape classes
 --
 --INSERT INTO s4_classrep.equishape_cfbm BY NAME
@@ -169,8 +204,8 @@ FROM file_download_landing.equi t;
 --    any_value(CASE WHEN t2.charid = 'TOP_SURFACE_AREA_M2' THEN TRY_CAST(t2.atwrt AS DECIMAL) ELSE NULL END) AS top_surface_area_m2,
 --    any_value(CASE WHEN t2.charid = 'WORKING_VOLUME_M3' THEN TRY_CAST(t2.atwrt AS DECIMAL) ELSE NULL END) AS working_volume_m3,
 --FROM s4_classrep.equi_masterdata t
---SEMI JOIN file_download.classequi t1 ON (t1.equi = t.equipment_id) AND t1.classname = 'SHCFBM'
---JOIN file_download.valuaequi t2 ON t2.equi = t.equipment_id
+--SEMI JOIN file_download_landing.classequi t1 ON (t1.equi = t.equipment_id) AND t1.classname = 'SHCFBM'
+--JOIN file_download_landing.valuaequi t2 ON t2.equi = t.equipment_id
 --GROUP BY equipment_id;
 --
 --INSERT INTO s4_classrep.equishape_cobm BY NAME
@@ -182,8 +217,8 @@ FROM file_download_landing.equi t;
 --    any_value(CASE WHEN t2.charid = 'SIDE_DEPTH_MM' THEN t2.atflv ELSE NULL END) AS side_depth_mm,
 --    any_value(CASE WHEN t2.charid = 'WORKING_VOLUME_M3' THEN TRY_CAST(t2.atwrt AS DECIMAL) ELSE NULL END) AS working_volume_m3,
 --FROM s4_classrep.equi_masterdata t
---SEMI JOIN file_download.classequi t1 ON (t1.equi = t.equipment_id) AND t1.classname = 'SHCOBM'
---JOIN file_download.valuaequi t2 ON t2.equi = t.equipment_id
+--SEMI JOIN file_download_landing.classequi t1 ON (t1.equi = t.equipment_id) AND t1.classname = 'SHCOBM'
+--JOIN file_download_landing.valuaequi t2 ON t2.equi = t.equipment_id
 --GROUP BY equipment_id;
 --
 --INSERT INTO s4_classrep.equishape_ecyl BY NAME
@@ -195,8 +230,8 @@ FROM file_download_landing.equi t;
 --    any_value(CASE WHEN t2.charid = 'MINOR_AXIS' THEN t2.atflv ELSE NULL END) AS minor_axis_mm,
 --    any_value(CASE WHEN t2.charid = 'WORKING_VOLUME_M3' THEN TRY_CAST(t2.atwrt AS DECIMAL) ELSE NULL END) AS working_volume_m3,
 --FROM s4_classrep.equi_masterdata t
---SEMI JOIN file_download.classequi t1 ON (t1.equi = t.equipment_id) AND t1.classname = 'SHECYL'
---JOIN file_download.valuaequi t2 ON t2.equi = t.equipment_id
+--SEMI JOIN file_download_landing.classequi t1 ON (t1.equi = t.equipment_id) AND t1.classname = 'SHECYL'
+--JOIN file_download_landing.valuaequi t2 ON t2.equi = t.equipment_id
 --GROUP BY equipment_id;
 --
 --INSERT INTO s4_classrep.equishape_hcyl BY NAME
@@ -207,8 +242,8 @@ FROM file_download_landing.equi t;
 --    any_value(CASE WHEN t2.charid = 'LENGTH_MM' THEN t2.atflv ELSE NULL END) AS length_mm,
 --    any_value(CASE WHEN t2.charid = 'WORKING_VOLUME_M3' THEN TRY_CAST(t2.atwrt AS DECIMAL) ELSE NULL END) AS working_volume_m3,
 --FROM s4_classrep.equi_masterdata t
---SEMI JOIN file_download.classequi t1 ON (t1.equi = t.equipment_id) AND t1.classname = 'SHHCYL'
---JOIN file_download.valuaequi t2 ON t2.equi = t.equipment_id
+--SEMI JOIN file_download_landing.classequi t1 ON (t1.equi = t.equipment_id) AND t1.classname = 'SHHCYL'
+--JOIN file_download_landing.valuaequi t2 ON t2.equi = t.equipment_id
 --GROUP BY equipment_id;
 --
 --INSERT INTO s4_classrep.equishape_miir BY NAME
@@ -225,8 +260,8 @@ FROM file_download_landing.equi t;
 --    any_value(CASE WHEN t2.charid = 'WIDTH_MM' THEN t2.atflv ELSE NULL END) AS width_mm,
 --    any_value(CASE WHEN t2.charid = 'WORKING_VOLUME_M3' THEN TRY_CAST(t2.atwrt AS DECIMAL) ELSE NULL END) AS working_volume_m3,
 --FROM s4_classrep.equi_masterdata t
---SEMI JOIN file_download.classequi t1 ON (t1.equi = t.equipment_id) AND t1.classname = 'SHMIIR'
---JOIN file_download.valuaequi t2 ON t2.equi = t.equipment_id
+--SEMI JOIN file_download_landing.classequi t1 ON (t1.equi = t.equipment_id) AND t1.classname = 'SHMIIR'
+--JOIN file_download_landing.valuaequi t2 ON t2.equi = t.equipment_id
 --GROUP BY equipment_id;
 --
 --INSERT INTO s4_classrep.equishape_rfbm BY NAME
@@ -239,8 +274,8 @@ FROM file_download_landing.equi t;
 --    any_value(CASE WHEN t2.charid = 'WIDTH_MM' THEN t2.atflv ELSE NULL END) AS width_mm,
 --    any_value(CASE WHEN t2.charid = 'WORKING_VOLUME_M3' THEN TRY_CAST(t2.atwrt AS DECIMAL) ELSE NULL END) AS working_volume_m3,
 --FROM s4_classrep.equi_masterdata t
---SEMI JOIN file_download.classequi t1 ON (t1.equi = t.equipment_id) AND t1.classname = 'SHRFBM'
---JOIN file_download.valuaequi t2 ON t2.equi = t.equipment_id
+--SEMI JOIN file_download_landing.classequi t1 ON (t1.equi = t.equipment_id) AND t1.classname = 'SHRFBM'
+--JOIN file_download_landing.valuaequi t2 ON t2.equi = t.equipment_id
 --GROUP BY equipment_id;
 --
 --INSERT INTO s4_classrep.equishape_rpbm BY NAME
@@ -253,8 +288,8 @@ FROM file_download_landing.equi t;
 --    any_value(CASE WHEN t2.charid = 'WIDTH_MM' THEN t2.atflv ELSE NULL END) AS width_mm,
 --    any_value(CASE WHEN t2.charid = 'WORKING_VOLUME_M3' THEN TRY_CAST(t2.atwrt AS DECIMAL) ELSE NULL END) AS working_volume_m3,
 --FROM s4_classrep.equi_masterdata t
---SEMI JOIN file_download.classequi t1 ON (t1.equi = t.equipment_id) AND t1.classname = 'SHRPBM'
---JOIN file_download.valuaequi t2 ON t2.equi = t.equipment_id
+--SEMI JOIN file_download_landing.classequi t1 ON (t1.equi = t.equipment_id) AND t1.classname = 'SHRPBM'
+--JOIN file_download_landing.valuaequi t2 ON t2.equi = t.equipment_id
 --GROUP BY equipment_id;
 --
 --INSERT INTO s4_classrep.equishape_rsbm BY NAME
@@ -268,8 +303,8 @@ FROM file_download_landing.equi t;
 --    any_value(CASE WHEN t2.charid = 'WIDTH_MM' THEN t2.atflv ELSE NULL END) AS width_mm,
 --    any_value(CASE WHEN t2.charid = 'WORKING_VOLUME_M3' THEN TRY_CAST(t2.atwrt AS DECIMAL) ELSE NULL END) AS working_volume_m3,
 --FROM s4_classrep.equi_masterdata t
---SEMI JOIN file_download.classequi t1 ON (t1.equi = t.equipment_id) AND t1.classname = 'SHRSBM'
---JOIN file_download.valuaequi t2 ON t2.equi = t.equipment_id
+--SEMI JOIN file_download_landing.classequi t1 ON (t1.equi = t.equipment_id) AND t1.classname = 'SHRSBM'
+--JOIN file_download_landing.valuaequi t2 ON t2.equi = t.equipment_id
 --GROUP BY equipment_id;
 --
 --INSERT INTO s4_classrep.equishape_scyl BY NAME
@@ -280,6 +315,6 @@ FROM file_download_landing.equi t;
 --    any_value(CASE WHEN t2.charid = 'SIDE_DEPTH_MM' THEN t2.atflv ELSE NULL END) AS side_depth_mm,
 --    any_value(CASE WHEN t2.charid = 'WORKING_VOLUME_M3' THEN TRY_CAST(t2.atwrt AS DECIMAL) ELSE NULL END) AS working_volume_m3,
 --FROM s4_classrep.equi_masterdata t
---SEMI JOIN file_download.classequi t1 ON (t1.equi = t.equipment_id) AND t1.classname = 'SHSCYL'
---JOIN file_download.valuaequi t2 ON t2.equi = t.equipment_id
+--SEMI JOIN file_download_landing.classequi t1 ON (t1.equi = t.equipment_id) AND t1.classname = 'SHSCYL'
+--JOIN file_download_landing.valuaequi t2 ON t2.equi = t.equipment_id
 --GROUP BY equipment_id;
