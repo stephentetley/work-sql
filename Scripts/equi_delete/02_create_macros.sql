bd2fe650d29ec31cb33a7b5152900e55f8d9b907
Stephen Tetley <stephen.tetley@gmail.com>
2025-11-15 17:49:52 +0000

    Work on equi_delete scripts

--- Scripts/equi_delete/02_create_macros.sql
+++ Scripts/equi_delete/02_create_macros.sql
@@ -4,6 +4,7 @@ CREATE OR REPLACE MACRO read_delete_worklist(xlsx_file) AS TABLE
 SELECT 
     t."S4 Equi Id" AS s4_equi_id,
     t."S4 Deleted Name" AS s4_deleted_name,
+    t."Batch" AS batch,
 FROM read_xlsx(xlsx_file :: VARCHAR, all_varchar=TRUE, sheet='Sheet1') AS t;
 
 
--- Scripts/equi_delete/04_data_copy_to_eu_equi_change.sql
+++ Scripts/equi_delete/04_data_copy_to_eu_equi_change.sql
@@ -8,6 +8,14 @@ SELECT
     'DISP' AS status_of_an_object,
 FROM s4_landing.equidelete_worklist t;
 
+DELETE FROM excel_uploader_equi_change.batch_worklist;
+INSERT INTO excel_uploader_equi_change.batch_worklist BY NAME
+SELECT 
+    t.s4_equi_id AS equi,
+    t.batch AS batch_number,
+FROM s4_landing.equidelete_worklist t;
+
+
 
 
 
