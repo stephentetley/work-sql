9adab086a276097ca31dcb0c1e68e2109cf90653
Stephen Tetley <stephen.tetley@gmail.com>
2026-02-16 17:07:33 +0000

    Work on ai2 to s4 classrep translation

---
+++ Scripts/equi_translate/01_translate_base_data.sql
@@ -0,0 +1,63 @@
+
+INSTALL rusty_sheet FROM community;
+LOAD rusty_sheet;
+
+CREATE SCHEMA IF NOT EXISTS equi_translate;
+
+CREATE OR REPLACE TABLE equi_translate.worklist (
+    ai2_reference VARCHAR NOT NULL,
+    batch INTEGER, 
+    equipment_transit_id VARCHAR,
+    s4_category	VARCHAR,
+    s4_object_type VARCHAR,
+    s4_class VARCHAR,
+    s4_name VARCHAR,
+    s4_floc	VARCHAR,
+    s4_superord_equipment VARCHAR,
+    s4_position INTEGER,
+    PRIMARY KEY(ai2_reference)
+);
+
+
+-- Worklist
+SELECT getvariable('equi_translate_worklist') AS equi_translate_worklist;
+
+INSERT OR REPLACE INTO equi_translate.worklist BY NAME
+SELECT
+    t."AI2 Record" AS ai2_reference,
+    t."Batch" AS batch, 
+    t."Equipment Transit ID" AS equipment_transit_id,
+    t."S4 Category" AS s4_category,
+    t."S4 Object Type" AS s4_object_type,
+    t."S4 Class" AS s4_class,
+    t."S4 Name" AS s4_name,
+    t."S4 Floc" AS s4_floc,
+    t."S4 Superord Equipment" AS s4_superord_equipment,
+    try_cast(t."S4 Position" AS INTEGER) AS s4_position,
+FROM read_sheet(
+    getvariable('equi_translate_worklist'), 
+    columns={
+        'Batch': 'INTEGER', 
+        'S4 Superord Equipment': 'VARCHAR'
+    }
+) t;
+
+
+
+-- TODO needs to check a worklist t
+
+-- -- INSERT OR REPLACE INTO s4_classrep.equiclass_metrel BY NAME
+-- SELECT
+--     t.ai2_reference AS equipment_id,
+--     -- t."Location On Site" AS location_on_site,
+--     -- t."HH_non HH site" AS metr_hh_meter,
+--     -- t."MPAN number" AS metr_mpan_number,
+--     -- 'TEMP_VALUE' AS uniclass_code,
+--     -- 'TEMP_VALUE' AS uniclass_desc,
+-- FROM ai2_classrep.equi_extra_masterdata t;
+
+-- DESCRIBE SELECT * FROM ai2_classrep.equi_extra_masterdata;
+
+-- DESCRIBE SELECT * FROM s4_classrep;
+
+
--- Scripts/equi_translate/02_translate_ai2_equiclass_to_s4.sql
+++ Scripts/equi_translate/02_translate_ai2_equiclass_to_s4.sql
@@ -1,16 +1,20 @@
 
 
--- TODO need to use a worklist as the destination type for
--- a particular source type is a user choice and not known
+-- We need to use a worklist as the destination type for a 
+-- particular source type is the user's choice and not known
 -- statically
 
+DELETE FROM s4_classrep.equiclass_metrel;
 INSERT OR REPLACE INTO s4_classrep.equiclass_metrel BY NAME
 SELECT
     t.ai2_reference AS equipment_id,
-    t."Location On Site" AS location_on_site,
-    t."HH_non HH site" AS metr_hh_meter,
-    t."MPAN number" AS metr_mpan_number,
+    t1."Location On Site" AS location_on_site,
+    t1."HH_non HH site" AS metr_hh_meter,
+    t1."MPAN number" AS metr_mpan_number,
     'TEMP_VALUE' AS uniclass_code,
     'TEMP_VALUE' AS uniclass_desc,
-FROM ai2_classrep.equiclass_electric_meter t;
+FROM equi_translate.worklist t
+JOIN ai2_classrep.equiclass_electric_meter t1 ON t1.ai2_reference = t.ai2_reference
+WHERE t.s4_class = 'METREL';
 
+SELECT * FROM s4_classrep.equiclass_metrel;
