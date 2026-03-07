bef5bc6033e20d932c291066fe4f5afeb4fe420c
Stephen Tetley <stephen.tetley@gmail.com>
2026-03-07 14:49:41 +0000

    Updated excel_uploader

--- Scripts/cli/make_excel_uploader.sql
+++ Scripts/cli/make_excel_uploader.sql
@@ -1,6 +1,7 @@
 
--- Just make tables and views - no dynamic loading of data
+-- Just make tables and views - no dynamic loading of data, so
+-- no env vars
 
-.read '../common/excel_uploader/01_setup_equi_create_tables.sql'
-.read '../common/excel_uploader/02_setup_equi_change_tables.sql'
-.read '../common/excel_uploader/03_setup_floc_create_tables.sql'
+.read '/home/stephen/_working/coding/work/work-sql/Scripts/common/excel_uploader/01_setup_equi_create_tables.sql'
+.read '/home/stephen/_working/coding/work/work-sql/Scripts/common/excel_uploader/02_setup_equi_change_tables.sql'
+.read '/home/stephen/_working/coding/work/work-sql/Scripts/common/excel_uploader/03_setup_floc_create_tables.sql'
