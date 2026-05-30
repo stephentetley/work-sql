61aab32562d55d73e97aa9e0f7e9afcf091d32bc
stephentetley <stephen.tetley@gmail.com>
2026-05-30 16:41:06 +0100

    Improved setup_excel_uploader Bash script with argument checking

--- Scripts2/cli/setup_excel_uploader.sh
+++ Scripts2/cli/setup_excel_uploader.sh
@@ -1,10 +1,21 @@
 #!/bin/bash
 
-duckdb << EOF
+if [ -z "$1" ]; then
+    echo "Supply the output SQLite filename as param $1"
+    return 1 
+fi
+
+if [[ ! -z "${WORK_SQL}" ]]; then
+    
+duckdb <<EOF
 attach '$1' as sqlite_db (type sqlite);
 .read '$WORK_SQL/Scripts2/excel_uploader/create_sqlite_tables.sql'
 detach sqlite_db
 EOF
 
-echo "Created $1"
+    echo "Created $1"
+else
+    echo "Must set the env var WORK_SQL and the param $1 to the SQLite output file"
+fi
+
 
