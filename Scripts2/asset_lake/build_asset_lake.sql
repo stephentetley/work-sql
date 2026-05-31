6a9f19c2b17f58d3bcb681b16d5500ba2e610b5b
stephentetley <stephen.tetley@gmail.com>
2026-05-31 20:42:46 +0100

    Added scripts to setup asset_lake

---
+++ Scripts2/asset_lake/build_asset_lake.sql
@@ -0,0 +1,130 @@
+--
+-- Copyright 2026 Stephen Tetley
+-- 
+-- Licensed under the Apache License, Version 2.0 (the "License");
+-- you may not use this file except in compliance with the License.
+-- You may obtain a copy of the License at
+-- 
+-- http://www.apache.org/licenses/LICENSE-2.0
+-- 
+-- Unless required by applicable law or agreed to in writing, software
+-- distributed under the License is distributed on an "AS IS" BASIS,
+-- WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+-- See the License for the specific language governing permissions and
+-- limitations under the License.
+-- 
+
+
+-- Preliminaries: 
+-- Must have asset_lake already attached and set as the default database 
+-- (by `use`)
+
+
+-- ztables
+
+create schema if not exists ztables;
+
+create or replace table ztables.eqobjl as
+select * from read_parquet(
+    getvariable('asset_lake_resources') || '/s4_ztables/ztable_eqobjl.parquet');
+
+create or replace table ztables.flobjl as
+select * from read_parquet(
+    getvariable('asset_lake_resources') || '/s4_ztables/ztable_flobjl.parquet');
+
+create or replace table ztables.flocdes as
+select * from read_parquet(
+    getvariable('asset_lake_resources') || '/s4_ztables/ztable_flocdes.parquet');
+
+create or replace table ztables.manuf as
+select * from read_parquet(
+    getvariable('asset_lake_resources') || '/s4_ztables/ztable_manuf.parquet');
+
+create or replace table ztables.objtype_manuf as
+select * from read_parquet(
+    getvariable('asset_lake_resources') || '/s4_ztables/ztable_objtype_manuf.parquet');
+
+-- site mapping
+
+create schema if not exists site_mapping;
+
+create or replace table site_mapping.site_mapping as
+select * from read_parquet(
+    getvariable('asset_lake_resources') || '/site_mapping/site_mapping.parquet');
+
+-- ai2 classlists
+
+create schema if not exists ai2_classlists;
+
+create or replace table ai2_classlists.ai2_equi_classes as
+select * from read_parquet(
+    getvariable('asset_lake_resources') || '/ai2_classlists/ai2_equi_classlist.parquet');
+
+-- s4 classlists
+
+create schema if not exists s4_classlists;
+
+create or replace table s4_classlists.s4_equi_classes as
+select * from read_parquet(
+    getvariable('asset_lake_resources') || '/s4_classlists/s4_equi_classlist.parquet');
+
+create or replace table s4_classlists.s4_equi_enums as
+select * from read_parquet(
+    getvariable('asset_lake_resources') || '/s4_classlists/s4_equi_classlist.parquet');
+
+create or replace table s4_classlists.s4_floc_classes as
+select * from read_parquet(
+    getvariable('asset_lake_resources') || '/s4_classlists/s4_floc_classlist.parquet');
+
+create or replace table s4_classlists.s4_floc_enums as
+select * from read_parquet(
+    getvariable('asset_lake_resources') || '/s4_classlists/s4_floc_classlist.parquet');
+
+
+-- ai2_masterdata
+
+create schema if not exists ai2_masterdata;
+
+create or replace table ai2_masterdata.ai2_floc as
+select * from read_parquet(
+    getvariable('asset_lake_resources') || '/ai2_masterdata/ai2_masterdata_floc.parquet');
+
+create or replace table ai2_masterdata.ai2_equi as
+select * from read_parquet(
+    getvariable('asset_lake_resources') || '/ai2_masterdata/ai2_masterdata_equi.parquet');
+
+create or replace table ai2_masterdata.ai2_sites as
+select * from read_parquet(
+    getvariable('asset_lake_resources') || '/ai2_masterdata/ai2_masterdata_site_simple.parquet');
+
+
+-- s4 masterdata
+
+create schema if not exists s4_masterdata;
+
+create or replace table s4_masterdata.s4_floc as
+select * from read_parquet(
+    getvariable('asset_lake_resources') || '/s4_masterdata/s4_masterdata_floc.parquet');
+
+create or replace table s4_masterdata.s4_equi as
+select * from read_parquet(
+    getvariable('asset_lake_resources') || '/s4_masterdata/s4_masterdata_equi.parquet');
+
+create or replace table s4_masterdata.s4_floc_east_north as
+select * from read_parquet(
+    getvariable('asset_lake_resources') || '/s4_masterdata/s4_masterdata_floc_east_north.parquet');
+
+create or replace table s4_masterdata.s4_equi_plinum as
+select * from read_parquet(
+    getvariable('asset_lake_resources') || '/s4_masterdata/s4_masterdata_equi_to_plinum.parquet');
+
+create or replace table s4_masterdata.s4_equi_sainum as
+select * from read_parquet(
+    getvariable('asset_lake_resources') || '/s4_masterdata/s4_masterdata_equi_to_sainum.parquet');
+
+
+
+
+
+
+
---
+++ Scripts2/cli/asset_lake.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+# Make sure to run this as a dot script...
+
+if ! { [ -n "$1" ] && [ -n "$2" ]; } ; then
+    echo "Supply the asset_lake name as param 1"
+    echo "Supply the resources root folder as param 2"
+    return 1 
+fi
+
+if [ ! -d "$2" ]; then
+    echo "Folder $2 does not exist."
+    return 1 
+fi
+
+
+if { [ ! -z "${WORK_SQL}" ] ; }; then
+    
+duckdb <<EOF
+
+attach 'ducklake:$1' as asset_lake;
+use asset_lake;
+
+set variable asset_lake_resources = '$2';
+.read '$WORK_SQL/Scripts2/asset_lake/build_asset_lake.sql'
+
+EOF
+
+    echo "Created $1"
+
+else
+    echo "Must set the env var WORK_SQL"
+fi
+
