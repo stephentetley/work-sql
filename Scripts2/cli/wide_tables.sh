f8e103b99c7b1be2bcb0d4a4dac67b9011783435
stephentetley <stephen.tetley@gmail.com>
2026-05-31 18:10:18 +0100

    Added Bash script to setup wide_tables

---
+++ Scripts2/cli/wide_tables.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+# Make sure to run this as a dot script...
+
+if [ -z "$1" ] ; then
+    dest="."
+else
+    dest=$1
+fi
+
+
+
+if { [ ! -z "${WORK_SQL}" ] && [ ! -z "${ASSET_LAKE_CONNECT_STRING}" ]; }; then
+    
+duckdb <<EOF
+attach '$ASSET_LAKE_CONNECT_STRING' as asset_lake (READ_ONLY);
+
+.read '$WORK_SQL/Scripts2/wide_tables/ai2_floc_wide_table.sql'
+.read '$WORK_SQL/Scripts2/wide_tables/ai2_equi_wide_table.sql'
+.read '$WORK_SQL/Scripts2/wide_tables/s4_floc_wide_table.sql'
+.read '$WORK_SQL/Scripts2/wide_tables/s4_equi_wide_table.sql'
+
+copy (select * from ai2_equi_wide_table) TO '$dest/ai2_equi_wide_table.parquet' (FORMAT parquet, COMPRESSION snappy);
+copy (select * from ai2_floc_wide_table) TO '$dest/ai2_floc_wide_table.parquet' (FORMAT parquet, COMPRESSION snappy);
+copy (select * from s4_equi_wide_table)  TO '$dest/s4_equi_wide_table.parquet' (FORMAT parquet, COMPRESSION snappy);
+copy (select * from s4_floc_wide_table)  TO '$dest/s4_floc_wide_table.parquet' (FORMAT parquet, COMPRESSION snappy);
+
+detach asset_lake;
+EOF
+    destfiles=`find "$dest" -maxdepth 1 -type f -iname "*wide_table.parquet"`
+
+    echo "Wrote:"
+    echo "$destfiles"
+
+else
+    echo "Must set the env vars WORK_SQL and ASSET_LAKE_CONNECT_STRING"
+fi
