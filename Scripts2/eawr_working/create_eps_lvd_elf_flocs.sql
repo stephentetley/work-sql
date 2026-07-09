f848bec1f901847aa304812dfdce6e50c738870f
stephentetley <stephen.tetley@gmail.com>
2026-07-09 17:05:20 +0100

    Making eawr wip script use asset_lake

--- Scripts2/eawr_working/create_eps_lvd_elf_flocs.sql
+++ Scripts2/eawr_working/create_eps_lvd_elf_flocs.sql
@@ -56,7 +56,7 @@ with cte1_max_level5 as (
     select 
         t.s4_site_floc, 
         coalesce(max(elf_index(t1.s4_functional_location)), 0) as max_level5,
-    from eawr.reported_locations t
+    from asset_lake.eawr.reported_location t
     join asset_lake.wide_tables.s4_floc t1 on t1.site_floc = t.s4_site_floc
     where t1.s4_category = 5 
     group by t.s4_site_floc   
@@ -65,7 +65,7 @@ with cte1_max_level5 as (
         t.s4_site_floc, 
         t.location as location,
         row_number() over (partition by t.s4_site_floc) as idx,
-    from eawr.reported_locations t
+    from asset_lake.eawr.reported_location t
 ), cte3_rescaled_numbered_locations as (
     select 
         t.s4_site_floc as site_root_floc, 
@@ -78,7 +78,7 @@ with cte1_max_level5 as (
         t.*,
         list(t1.db_or_panels) as sld_paths,
     from cte3_rescaled_numbered_locations t
-    join eawr.reported_locations t1 on t1.location = t.location and t1.s4_site_floc = t.site_root_floc
+    join asset_lake.eawr.reported_location t1 on t1.location = t.location and t1.s4_site_floc = t.site_root_floc
     group by all
 ), cte5_new_flocs as (
     select 
